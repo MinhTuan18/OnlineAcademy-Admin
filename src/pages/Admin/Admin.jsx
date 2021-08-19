2ad166f1d2549cd7f242ca3a56d8852b50dca88a
Tuan Minh Ho <minhtuan.t1181@gmail.com>
2021-08-19 15:44:35 +0700

    update menu icon + edit sub category

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 import Auth from './SignIn/SignIn';
 import { API_PORT } from '../../utils/constants';
 import { CategoryCreate, CategoryEdit, CategoryList, CategoryShow } from './Categories/Categories';
-import CategoryIcon from '@material-ui/icons/Category';
+import AccountTreeIcon from '@material-ui/icons/AccountTree';
+import ListIcon from '@material-ui/icons/List';
+import PeopleIcon from '@material-ui/icons/People';
+import MenuBookIcon from '@material-ui/icons/MenuBook';
+import FaceIcon from '@material-ui/icons/Face';
 import { UserCreate, UserEdit, UserList, UserShow } from './Users/Users';
 import { InstructorCreate, InstructorEdit, InstructorList, InstructorShow } from './Instructors/Instructors';
 import { CourseEdit, CourseList, CourseShow } from './Courses/Courses';
@@ -23,11 +27,11 @@ const httpClient = (url, options = {}) => {
 const AdminComponent = () => (
   <>
     <Admin title="Table Planner Admin" restClient={jsonServerRestClient(`${API_PORT}`, httpClient)} authClient={Auth}>
-      <Resource name="categories" list={CategoryList} show={CategoryShow} edit={CategoryEdit} icon={CategoryIcon} create={CategoryCreate} remove={Delete}/>
-      <Resource name="subCategories" list={SubCategoryList} edit={SubCategoryEdit} show={SubCategoryShow} create={SubCategoryCreate} remove={Delete}/>
-      <Resource name="students" list={UserList}  edit={UserEdit} show={UserShow} create={UserCreate}/>
-      <Resource name="instructors" list={InstructorList} edit={InstructorEdit} create={InstructorCreate} show={InstructorShow}/>
-      <Resource name="courses" list={CourseList} edit={CourseEdit} show={CourseShow}/>
+      <Resource name="categories" list={CategoryList} show={CategoryShow} edit={CategoryEdit} icon={ListIcon} create={CategoryCreate} remove={Delete}/>
+      <Resource name="subCategories" list={SubCategoryList} edit={SubCategoryEdit} show={SubCategoryShow} create={SubCategoryCreate} remove={Delete} icon={AccountTreeIcon}/>
+      <Resource name="students" list={UserList}  edit={UserEdit} show={UserShow} create={UserCreate} icon={FaceIcon}/>
+      <Resource name="instructors" list={InstructorList} edit={InstructorEdit} create={InstructorCreate} show={InstructorShow} icon={PeopleIcon}/>
+      <Resource name="courses" list={CourseList} edit={CourseEdit} show={CourseShow} icon={MenuBookIcon}/>
     </Admin>
   </>
 );
--- src/pages/Admin/SubCategories/SubCategories.jsx
+++ src/pages/Admin/SubCategories/SubCategories.jsx
@@ -13,7 +13,7 @@ import {
   Filter,
   ReferenceArrayField,
   Create,
-  required, ChipField, DeleteButton
+  required, ChipField, DeleteButton, ReferenceInput, SelectInput, BooleanField, NumberField
 } from 'admin-on-rest';
 
 const validateName = [required];
@@ -28,8 +28,9 @@ export const SubCategoryList = (props) => (
   <List {...props} filters={<SubCategoryFilter />} perPage={10}>
     <Datagrid>
       <TextField label="Name" source="name" />
-      <ChipField label="Category" source="category.name" />
-      
+      <ReferenceField source="category" reference="categories">
+        <ChipField label="Category" source="name" />
+      </ReferenceField>
       <EditButton label="Edit" />
       <DeleteButton label="Delete"/>
     </Datagrid>
@@ -40,13 +41,17 @@ export const SubCategoryShow = (props) => (
   <Show title="Sub Category Details" {...props}>
     <SimpleShowLayout>
       <TextField label="Name" source="name" />
+      <ReferenceField source="category" reference="categories">
+        <ChipField label="Category" source="name" />
+      </ReferenceField>
       <ReferenceArrayField label="Courses" reference="courses" source="courses">
         <Datagrid>
-          <TextField label="Title" source="title" />
-          <ReferenceField source="instructor" reference="instructors">
-            <TextField label="Instructor" source="name" />
-          </ReferenceField>
-          <TextField label="Status" source="status" />
+        <TextField label="Title" source="title" />
+        <TextField label="Instructor" source="instructor.name" />
+        <ChipField label="Sub Category" source="subCategory.name" />
+        <TextField label="Status" source="status" />
+        <BooleanField label="Is blocked" source="isBlocked"/>
+        <NumberField label="Rating" source="averageRating" />
           <EditButton label="Edit" />
         </Datagrid>
       </ReferenceArrayField>
@@ -58,6 +63,9 @@ export const SubCategoryEdit = (props) => (
   <Edit title="Edit Sub Category" {...props}>
     <SimpleForm>
       <TextInput label="Name" source="name" />
+      <ReferenceInput label="Category" source="category" reference="categories">
+        <SelectInput optionText="name" validate={validateName}/>
+      </ReferenceInput>
     </SimpleForm>
   </Edit>
 );
@@ -66,6 +74,9 @@ export const SubCategoryCreate = (props) => (
   <Create title="Create Sub Category" {...props}>
     <SimpleForm>
       <TextInput label="Name" source="name" validate={validateName}/>
+      <ReferenceInput label="Category" source="category" reference="categories">
+        <SelectInput optionText="name" validate={validateName}/>
+      </ReferenceInput>
     </SimpleForm>
   </Create>
 );
